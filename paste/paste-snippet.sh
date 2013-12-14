3298f58e71b5ac52e33ad5e2fee24aaa32c6fcc0
Louis Roché <louis@louisroche.net>
2013-12-14 14:11:56 +0800

    paste snippet

---
+++ paste/paste-snippet.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage () {
+  echo "gimmeh a path dumb fuck"
+}
+
+header=$HOME/bin/pastes/header.html
+footer=$HOME/bin/pastes/footer.html
+
+if [ $# -ne 1 ] || [ ! -f "$1" ]; then
+    usage
+else
+    filename1=$(mktemp -t "$1")
+    filename2=$(mktemp -t "$1")
+    dst=$(basename "$1").html
+    chmod +r "$filename1"
+    chmod +r "$filename2"
+    pygmentize -f html -O linenos,encoding=utf8 -o "$filename1" "$1"
+    cat $header "$filename1" $footer > "$filename2"
+    scp "$filename2" khady@halva.khady.info:public_html/pastes/"$dst" > /dev/null
+    scp "$1" khady@halva.khady.info:public_html/pastes/"$1" > /dev/null
+    rm "$filename1"
+    rm "$filename2"
+    echo "http://halva.khady.info/~khady/pastes/$dst"
+fi
