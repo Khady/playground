d9f4ed046451ac793649b479a55a511293198536
Louis Roché <louis@louisroche.net>
2013-12-14 18:44:22 +0800

    basename

--- paste/paste-snippet.sh
+++ paste/paste-snippet.sh
@@ -8,18 +8,19 @@ header=$HOME/bin/pastes/header.html
 footer=$HOME/bin/pastes/footer.html
 
 if [ $# -ne 1 ] || [ ! -f "$1" ]; then
-    usage
+  usage
 else
-    filename1=$(mktemp -t "$1")
-    filename2=$(mktemp -t "$1")
-    dst=$(basename "$1").html
-    chmod +r "$filename1"
-    chmod +r "$filename2"
-    pygmentize -f html -O linenos,encoding=utf8 -o "$filename1" "$1"
-    cat $header "$filename1" $footer > "$filename2"
-    scp "$filename2" khady@halva.khady.info:public_html/pastes/"$dst" > /dev/null
-    scp "$1" khady@halva.khady.info:public_html/pastes/"$1" > /dev/null
-    rm "$filename1"
-    rm "$filename2"
-    echo "http://halva.khady.info/~khady/pastes/$dst"
+  filename=$(basename "$1")
+  filename1=$(mktemp -t "$filename")
+  filename2=$(mktemp -t "$filename")
+  dst="$filename".html
+  chmod +r "$filename1"
+  chmod +r "$filename2"
+  pygmentize -f html -O linenos,encoding=utf8 -o "$filename1" "$1"
+  cat $header "$filename1" $footer > "$filename2"
+  scp "$filename2" khady@halva.khady.info:public_html/pastes/"$dst" > /dev/null
+  scp "$1" khady@halva.khady.info:public_html/pastes/"$filename" > /dev/null
+  rm "$filename1"
+  rm "$filename2"
+  echo "http://halva.khady.info/~khady/pastes/$dst"
 fi
